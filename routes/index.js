fe88583680c4f9c03776541dc19307e89ecce49c
Neelima Reddy <neelimareddy@Neelimas-MacBook-Pro.local>
2017-10-25 20:44:47 -0700

    done

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 //**************************************************************************
 //***** mongodb get all of the Routes in Routes collection where frequence>=1
 //      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
-app.get('/mongodb', function (request, response) {
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+
+router.get('/mongodb', function (request, response) {
 
     mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
         if(err) throw err;
-//get collection of routes
+        //get collection of routes
         var Routes = db.collection('Routes');
-//get all Routes with frequency >=1
+        //get all Routes with frequency >=1
         Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
             if(err) throw err;
 
@@ -15,9 +19,11 @@ app.get('/mongodb', function (request, response) {
 
         });
 
-//close connection when your app is terminating.
+        //close connection when your app is terminating.
         db.close(function (err) {
             if(err) throw err;
         });
     });//end of connect
 });//end app.get
+
+module.exports = router;
